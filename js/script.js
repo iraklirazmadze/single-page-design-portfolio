a10a345117f22d333bdd70878ea419d16f19f40f
razma <iraklirazmadze9@gmail.com>
2022-08-04 13:03:42 +0400

    finished

--- js/script.js
+++ js/script.js
@@ -1,34 +1,46 @@
 let leftArrow = document.querySelector('.arrows').children[0];
 let rightArrow = document.querySelector('.arrows').children[1];
+let isMoving = false;
 leftArrow.addEventListener('click', () =>{
-  let imgs = document.querySelector(".images");
-  imgs.lastElementChild.style.width = "0px";
-  let goalImg = imgs.children[4];
-  setTimeout(() => {
-    imgs.insertBefore(goalImg,imgs.firstChild);
-  }, 200);
-  setTimeout(() => {
-    imgs.children[0].style.width = "78%";
-  }, 400);
- 
-  
-
-  
-  
- 
-  
- 
-
+  if(isMoving == true){
+    return;
+  }else{
+    isMoving = true;
+    let imgs = document.querySelector(".images");
+    let goalImg = imgs.children[4];
+    let newImg = document.createElement('img');
+    newImg.style.width = '0px';
+    newImg.src = document.querySelector(".images").children[4].src
+    imgs.insertBefore(newImg,imgs.firstChild);
+    setTimeout(() => {
+      imgs.lastElementChild.style.width = "0px";
+      imgs.firstElementChild.style.width = '78%';
+    }, 10);
+    setTimeout(() => {
+      imgs.lastElementChild.remove();
+      isMoving = false;
+    }, 301);
+  }
 })
 
-rightArrow.addEventListener('click', () => {
-  let imgs = document.querySelector(".images");
-  imgs.firstElementChild.style.width = '0px'
-  setTimeout(() => {
-    imgs.appendChild(imgs.children[0]);
-  }, 200);
-  setTimeout(() => {
-    imgs.children[4].style.width = "78%";
-  }, 400);
+rightArrow.addEventListener('click', () =>{
+  if(isMoving == true){
+      return;
+    }else{
+      isMoving = true;
+    let imgs = document.querySelector(".images");
+    let goalImg = imgs.children[0];
+    let newImg = document.createElement('img');
+    newImg.style.width = '0px';
+    newImg.src = document.querySelector(".images").children[0].src
+    imgs.insertBefore(newImg,imgs.lastChild);
+    setTimeout(() => {
+      imgs.firstElementChild.style.width = "0px";
+      newImg.style.width = '78%';
+    }, 10);
+    setTimeout(() => {
+      imgs.firstElementChild.remove();
+      isMoving =  false;
+    }, 301);
+  }
 })
-
